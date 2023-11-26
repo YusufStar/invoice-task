4ab63fef5b5f827467e4865334e80df00ff49cd5
yusuf <ysfyldz0077@gmail.com>
2023-11-26 20:07:48 +0300

    work

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -3,30 +3,12 @@ import FileInput from '@/components/FileInput';
 import TextInput from '@/components/TextInput';
 import CoordinateIndicator from '@/components/CoordinateIndicator';
 import React, { useEffect, useRef, useState } from 'react'
+import useCanvas from '@/hooks/canvas';
 
 const Admin = () => {
   const [fileUrl, setFileUrl] = useState(null);
   const canvasRef = useRef();
-  const [data, setData] = useState({
-    inputs: {
-      address: "",
-      date: "",
-      total: ""
-    },
-    coordinates: {
-      total: null,
-      address: null,
-      date: null
-    },
-    activeInput: "",
-    width: 0,
-    height: 0
-  });
-  const [isCoordinateIndicatorVisible, setCoordinateIndicatorVisible] = useState(false);
-
-  const handleCoordinateButtonClick = () => {
-    setCoordinateIndicatorVisible(true);
-  };
+  const { drawSquare, drawImage, drawText, data, setData, isCoordinateIndicatorVisible, setCoordinateIndicatorVisible, clearData, handleInputChange, handleDownload, resetCanvas } = useCanvas(canvasRef)
 
   const handleImageUpload = (event) => {
     const input = event.target;
@@ -46,121 +28,32 @@ const Admin = () => {
 
   useEffect(() => {
     if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      canvas.addEventListener('click', handleCanvasClick);
-    }
-
-    return () => {
-      if (canvasRef.current) {
-        canvasRef.current.removeEventListener('click', handleCanvasClick);
-      }
-    };
-  }, [canvasRef, data, data.activeInput]);
-
-  function drawEmptySquare(canvas, x, y, width, height, color) {
-    const context = canvas.getContext('2d');
-
-    context.strokeStyle = color || '#000';
-    context.strokeRect(x, y, width, height);
-  }
-
-  useEffect(() => {
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
-      context.clearRect(0, 0, canvas.width, canvas.height);
-
       if (fileUrl) {
+        // canvası resetle
+        resetCanvas();
+
+        // Image oluştur
         const img = new Image();
         img.src = fileUrl;
+
         img.onload = () => {
+          const canvas = canvasRef.current
+          const context = canvas.getContext('2d')
+
+          // resimi yazdır - bir sorun nedeniyle hooktadki fonksiyonu kullanamayıorum çalışmıyor sadece bu kısımda
           context.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+          // Kareleri tüm koordinatlara çiz
           Object.entries(data.coordinates).forEach(([key, value]) => {
             if (value) {
-              drawEmptySquare(canvas, value.x, value.y, 32, 12, "#ce3fad");
+              drawSquare(value.x, value.y, 32, 12, "#ce3fad");
             }
           });
         };
       }
     }
-  }, [data.coordinates])
+  }, [data.coordinates, fileUrl]);
 
-  const handleCanvasClick = (e) => {
-    const rect = canvasRef.current.getBoundingClientRect();
-    const x = e.clientX - rect.left - 12;
-    const y = e.clientY - rect.top - 12;
-    setCoordinateIndicatorVisible(false)
-
-    setData((prev) => ({
-      ...prev,
-      coordinates: {
-        ...prev.coordinates,
-        [prev.activeInput]: { x, y }
-      }
-    }));
-  };
-
-  const clearCanvas = () => {
-    setData((prev) => ({
-      ...prev,
-      coordinates: {
-        total: null,
-        address: null,
-        date: null
-      },
-      inputs: {
-        address: "",
-        date: "",
-        total: ""
-      }
-    }));
-
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (fileUrl) {
-      const img = new Image();
-      img.src = fileUrl;
-      img.onload = () => {
-        context.drawImage(img, 0, 0, canvas.width, canvas.height);
-      };
-    }
-  };
-
-  const convertCanvasToDataURL = () => {
-    const canvas = canvasRef.current;
-    const dataURL = canvas.toDataURL("image/png");
-    return dataURL
-  };
-
-  const handleDownload = () => {
-    const url = convertCanvasToDataURL()
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = "invoice.png";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
-  const drawText = (text, x, y) => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    context.fillStyle = "#000";
-    context.font = "12px Arial";
-    context.fillText(text, x, y);
-  };
-
-  const handleInputChange = (key, value) => {
-    setData((prev) => ({
-      ...prev,
-      inputs: {
-        ...prev.inputs,
-        [key]: value
-      }
-    }));
-  };
 
   return (
     <div className='flex flex-col items-center bg-gray-950 w-screen h-screen gap-4 p-12 relative'>
@@ -173,15 +66,23 @@ const Admin = () => {
 
         {fileUrl && <form onSubmit={(e) => {
           e.preventDefault();
-          const canvas = canvasRef.current;
-          const context = canvas.getContext('2d');
-          context.clearRect(0, 0, canvas.width, canvas.height);
 
           if (fileUrl) {
+            // canvası resetle
+            resetCanvas();
+
+            // Image oluştur
             const img = new Image();
             img.src = fileUrl;
+
             img.onload = () => {
+              const canvas = canvasRef.current
+              const context = canvas.getContext('2d')
+
+              // resimi yazdır - bir sorun nedeniyle hooktadki fonksiyonu kullanamayıorum çalışmıyor sadece bu kısımda
               context.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+              // textleri tüm koordinatlara çiz
               Object.entries(data.inputs).forEach(([inputKey, inputValue]) => {
                 drawText(inputValue, data.coordinates?.[inputKey]?.x, data.coordinates?.[inputKey]?.y + 8);
               });
@@ -203,7 +104,7 @@ const Admin = () => {
                   className='ml-4 px-2 py-2 bg-gray-300 text-black rounded-md transition-all duration-200 hover:opacity-75'
                   onClick={() => {
                     setData((prev) => ({ ...prev, activeInput: key }))
-                    handleCoordinateButtonClick()
+                    setCoordinateIndicatorVisible(true)
                   }}
                 >
                   <svg fill="#000000" width="24" height="24" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg"><title /><path d="M21.31,52.45h-.17a2,2,0,0,1-1.82-2.16l2.87-34.72a2,2,0,1,1,4,.33L23.3,50.61A2,2,0,0,1,21.31,52.45Z" /><path d="M36.82,50.28l-.24,0A2,2,0,0,1,34.83,48l.33-2.71a2,2,0,1,1,4,.48l-.32,2.7A2,2,0,0,1,36.82,50.28Z" /><path d="M38,36.62h-.17A2,2,0,0,1,36,34.46L37.7,13.39a2,2,0,1,1,4,.33L39.94,34.79A2,2,0,0,1,38,36.62Z" /><path d="M46.78,27.68h-.17L11.89,24.8a2,2,0,0,1-1.83-2.16,2,2,0,0,1,2.16-1.82l34.72,2.87a2,2,0,0,1-.16,4Z" /><path d="M49,43.19h-.17L14.06,40.31a2,2,0,0,1-1.83-2.16,2,2,0,0,1,2.16-1.82L49.11,39.2a2,2,0,0,1-.16,4Z" /></svg>
@@ -222,7 +123,11 @@ const Admin = () => {
           </button>
           <button
             type='button'
-            onClick={() => clearCanvas()}
+            onClick={() => {
+              clearData()
+              resetCanvas()
+              drawImage(fileUrl)
+            }}
             className='px-4 py-2 transition-all duration-200 hover:opacity-75 w-auto h-auto bg-gray-300 rounded-md text-black'
           >
             Clear Canvas
@@ -241,12 +146,6 @@ const Admin = () => {
       </div>
 
       <FileInput onChange={handleImageUpload} />
-
-      <img src={fileUrl} className='w-[450px] h-fit hidden' onLoad={(e) => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        context.drawImage(e.currentTarget, 0, 0, 450, 636);
-      }} />
     </div>
   );
 }
---
+++ hooks/canvas.js
@@ -0,0 +1,152 @@
+'use client'
+import { useEffect, useRef, useState } from 'react';
+
+const useCanvas = (canvasRef) => {
+  const [data, setData] = useState({
+    inputs: {
+      address: "",
+      date: "",
+      total: ""
+    },
+    coordinates: {
+      total: null,
+      address: null,
+      date: null
+    },
+    activeInput: "",
+    width: 0,
+    height: 0
+  });
+  const [isCoordinateIndicatorVisible, setCoordinateIndicatorVisible] = useState(false);
+
+  // kare çizme fonksiyonu
+  const drawSquare = (x, y, width, height, color) => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    context.strokeStyle = color || '#000';
+    context.fillStyle = color || '#000'; // Set fill style
+    context.strokeRect(x, y, width, height);
+  };
+
+  // daire çizme
+  const drawCircle = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    context.beginPath();
+    context.arc(200, 200, 50, 0, 2 * Math.PI);
+    context.fill();
+  };
+
+  // resimi canvasa bastırma
+  const drawImage = (imageUrl) => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = () => {
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+
+    return img
+  };
+
+  // verilen texti verilen koordinatlara yazdır
+  const drawText = (text, x, y) => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.fillStyle = "#000";
+    context.font = "12px Arial";
+    context.fillText(text, x, y+3);
+  };
+
+  // canvasa tıklanıldığında koordinatı al
+  const handleCanvasClick = (e) => {
+    const rect = canvasRef.current.getBoundingClientRect();
+    const x = e.clientX - rect.left - 12;
+    const y = e.clientY - rect.top - 12;
+    setCoordinateIndicatorVisible(false)
+
+    if (data.activeInput) {
+      setData((prev) => ({
+        ...prev,
+        coordinates: {
+          ...prev.coordinates,
+          [prev.activeInput]: { x, y }
+        }
+      }));
+    }
+  };
+
+  // canvası dinle tıklanıldığında çalışacak fonksiyon!
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      canvas.addEventListener('click', handleCanvasClick);
+    }
+
+    return () => {
+      if (canvasRef.current) {
+        canvasRef.current.removeEventListener('click', handleCanvasClick);
+      }
+    };
+  }, [canvasRef, data, data.activeInput]);
+
+  // canvası temizle tamamen!
+  const clearData = () => {
+    setData((prev) => ({
+      ...prev,
+      coordinates: {
+        total: null,
+        address: null,
+        date: null
+      },
+      inputs: {
+        address: "",
+        date: "",
+        total: ""
+      }
+    }));
+  };
+
+  const resetCanvas = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  // inputlara girdi girildiğinde datayı değiştir
+  const handleInputChange = (key, value) => {
+    setData((prev) => ({
+      ...prev,
+      inputs: {
+        ...prev.inputs,
+        [key]: value
+      }
+    }));
+  };
+
+  // canvası dataya çevir
+  const convertCanvasToDataURL = () => {
+    const canvas = canvasRef.current;
+    const dataURL = canvas.toDataURL("image/png");
+    return dataURL
+  };
+
+  // çevrilen datayı indir
+  const handleDownload = () => {
+    const url = convertCanvasToDataURL()
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "invoice.png";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return { drawSquare, drawCircle, drawImage, drawText, data, setData, isCoordinateIndicatorVisible, setCoordinateIndicatorVisible, handleInputChange, clearData, handleDownload, resetCanvas };
+};
+
+export default useCanvas;
